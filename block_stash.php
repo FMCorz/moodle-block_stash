216109631cde38be7fcdfe201831a1e8715f7bbc
Adrian Greeve <adrian@moodle.com>
2016-06-13 11:08:13 +0800

    block_stash : Initial files

---
+++ block_stash.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Classes for block_stash
+ *
+ * @package    block_stash
+ * @copyright  2016 Adrian Greeve <adrian@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class block_stash extends block_base {
+
+    /**
+     * Core function used to initialize the block.
+     */
+    public function init() {
+        $this->title = get_string('pluginname', 'block_stash');
+    }
+
+    /**
+     * Applicable formats.
+     *
+     * @return array
+     */
+    public function applicable_formats() {
+        return array('course' => true, 'my' => true);
+    }
+
+    /**
+     * Get content.
+     *
+     * @return stdClass
+     */
+    public function get_content() {
+
+        $this->content = new stdClass();
+
+        $renderer = $this->page->get_renderer('block_stash');
+        $page = new \block_stash\output\render_helper();
+        $this->content->text = $renderer->render($page);
+        $this->content->footer = '';
+
+        return $this->content;
+
+    }
+}
---
+++ classes/output/renderer.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Block Stash renderer.
+ *
+ * @package    block_stash
+ * @copyright  2016 Adrian Greeve <adrian@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_stash\output;
+defined('MOODLE_INTERNAL') || die();
+
+use plugin_renderer_base;
+use renderable;
+/**
+ * Block Stash renderer class.
+ *
+ * @package    block_stash
+ * @copyright  2016 Adrian Greeve
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_stash_renderer extends plugin_renderer_base {
+
+    public function main_content(renderable $page) {
+        $data = $page->export_for_template($this);
+        return parent::render_from_template('block_stash/main_content', $data);
+    }
+
+}
