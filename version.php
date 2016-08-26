fe9e6b7d2edeaaa3c0321008545e2811f34b7aea
Frederic Massart <fred@moodle.com>
2016-08-26 15:19:02 +0800

    Bump to v1.1.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016082600;
+$plugin->version   = 2016082601;
 $plugin->requires  = 2015051100;
 $plugin->component = 'block_stash';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.1.0';
