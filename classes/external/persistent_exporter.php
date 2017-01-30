d50979d228996ce9a39cb176ca8103464c1fea64
Adrian Greeve <adrian@moodle.com>
2017-01-30 11:05:42 +0800

    Update to persistence classes.

--- classes/external/persistent_exporter.php
+++ classes/external/persistent_exporter.php
@@ -28,65 +28,80 @@ defined('MOODLE_INTERNAL') || die();
 // Requirements for core_competency\external\persistent_exporter copy/paste.
 use coding_exception;
 
-// At the moment we rely on the core_competency.
-// Add compability with < 3.1 where the class does not exist.
-if (!class_exists('core_competency\\external\\persistent_exporter')) {
+if (!class_exists('core\\external\\persistent_exporter')) {
 
-    require_once($CFG->libdir . '/externallib.php');
+    // At the moment we rely on the core_competency.
+    // Add compability with < 3.1 where the class does not exist.
+    if (!class_exists('core_competency\\external\\persistent_exporter')) {
 
-    /**
-     * Copy/paste from core_competency\external\persistent_exporter.
-     */
-    abstract class persistent_exporter extends exporter {
-
-        /** @var \core_competency\persistent The persistent object we will export. */
-        protected $persistent = null;
+        require_once($CFG->libdir . '/externallib.php');
 
         /**
-         * Constructor - saves the persistent object, and the related objects.
-         *
-         * @param \core_competency\persistent $persistent The persistent object to export.
-         * @param array $related - An optional list of pre-loaded objects related to this persistent.
+         * Copy/paste from core_competency\external\persistent_exporter.
          */
-        public final function __construct(\block_stash\persistent $persistent, $related = array()) {
-            $classname = static::define_class();
-            if (!$persistent instanceof $classname) {
-                throw new coding_exception('Invalid type for persistent. ' .
-                                           'Expected: ' . $classname . ' got: ' . get_class($persistent));
+        abstract class persistent_exporter extends exporter {
+
+            /** @var \core_competency\persistent The persistent object we will export. */
+            protected $persistent = null;
+
+            /**
+             * Constructor - saves the persistent object, and the related objects.
+             *
+             * @param \core_competency\persistent $persistent The persistent object to export.
+             * @param array $related - An optional list of pre-loaded objects related to this persistent.
+             */
+            public final function __construct(\block_stash\persistent $persistent, $related = array()) {
+                $classname = static::define_class();
+                if (!$persistent instanceof $classname) {
+                    throw new coding_exception('Invalid type for persistent. ' .
+                                               'Expected: ' . $classname . ' got: ' . get_class($persistent));
+                }
+                $this->persistent = $persistent;
+
+                if (method_exists($this->persistent, 'get_context') && !isset($this->related['context'])) {
+                    $this->related['context'] = $this->persistent->get_context();
+                }
+
+                $data = $persistent->to_record();
+                parent::__construct($data, $related);
             }
-            $this->persistent = $persistent;
 
-            if (method_exists($this->persistent, 'get_context') && !isset($this->related['context'])) {
-                $this->related['context'] = $this->persistent->get_context();
+            /**
+             * Persistent exporters get their standard properties from the persistent class.
+             *
+             * @return array Keys are the property names, and value their definition.
+             */
+            protected final static function define_properties() {
+                $classname = static::define_class();
+                return $classname::properties_definition();
             }
 
-            $data = $persistent->to_record();
-            parent::__construct($data, $related);
+            /**
+             * Returns the specific class the persistent should be an instance of.
+             *
+             * @return string
+             */
+            protected static function define_class() {
+                throw new coding_exception('define_class() must be overidden.');
+            }
+
         }
+    } else {
+        // When this class will be in core we'll depend on it.
 
         /**
-         * Persistent exporters get their standard properties from the persistent class.
+         * Persistent exporter class.
          *
-         * @return array Keys are the property names, and value their definition.
+         * @package    block_stash
+         * @copyright  2016 Frédéric Massart - FMCorz.net
+         * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
          */
-        protected final static function define_properties() {
-            $classname = static::define_class();
-            return $classname::properties_definition();
-        }
-
-        /**
-         * Returns the specific class the persistent should be an instance of.
-         *
-         * @return string
-         */
-        protected static function define_class() {
-            throw new coding_exception('define_class() must be overidden.');
+        class persistent_exporter extends \core_competency\external\persistent_exporter {
         }
 
     }
-} else {
-    // When this class will be in core we'll depend on it.
 
+} else {
     /**
      * Persistent exporter class.
      *
@@ -94,7 +109,6 @@ if (!class_exists('core_competency\\external\\persistent_exporter')) {
      * @copyright  2016 Frédéric Massart - FMCorz.net
      * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
      */
-    class persistent_exporter extends \core_competency\external\persistent_exporter {
-    }
-
+    class persistent_exporter extends \core\external\persistent_exporter {
+    }    
 }
